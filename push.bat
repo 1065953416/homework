7fa39e28a8b3cdf48d7a5cd29b2746d249e2bc22
Zhang Muzi <1065953416@qq.com>
2023-12-13 19:03:55 +0800

    自动推送

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
-git config --global user.name 'yangjh-xbmu'
-git config --global user.email 'yangjh@xbmu.edu.cn'
+git config --global user.name 'Zhang Muzi'
+git config --global user.email '1065953416@qq.com'
 git config --global http.sslverify false
 git add .
 git commit -m '自动推送'
